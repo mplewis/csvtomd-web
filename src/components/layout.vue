89527aef2139fb56f3ff1e714a27537245f408df
Matt Lewis <matt@mplewis.com>
2018-08-19 22:25:21 -0600

    feat: csv parsing

---
+++ src/components/layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <textarea v-model="input" />
+    <pre><code>{{output}}</code></pre>
+  </div>
+</template>
+
+<script>
+import { fromString } from 'csvtomd-lib'
+
+export default {
+  name: 'Layout',
+  data: () => ({ input: '' }),
+  computed: { output () { return fromString(this.input) } }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
